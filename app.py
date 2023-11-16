1a524bae764ad2243d7e3a1a02ca2a83d6718a0c
Petar Canic <p.canic@eclecticiq.com>
2023-11-16 12:16:02 +0100

    Deploying app

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from resources_tag import blp as TagBlueprint
 from resource_auth import blp as AuthBlueprint
 from db import db
 from flask_jwt_extended import JWTManager
+from flask_migrate import Migrate
 import models
 from blacklist import BLOCKLIST
 
@@ -13,6 +14,15 @@ from blacklist import BLOCKLIST
 # namena da se sve lepo setuje i da se kaznije mozda prebaci
 # na 2 verzije jedna za produkciju 2 za unapredjivanje aplikacije!!!
 # ja sam ovde bazu bacio na docker POSTGRES je baza !!!
+
+
+#alembic ako smo imali ranije bazu moramo da je izbrisemo
+#da se ponovo krene sve !!! inace ce da se zali
+
+pg_pass = "7OqRu1iHlVeLB7O8suzjZRl8MUxqGkeN"
+internal_url = "postgres://test:7OqRu1iHlVeLB7O8suzjZRl8MUxqGkeN@dpg-clavig616hkc73816s70-a/test_pan1"
+external_url = "postgres://test:7OqRu1iHlVeLB7O8suzjZRl8MUxqGkeN@dpg-clavig616hkc73816s70-a.frankfurt-postgres.render.com/test_pan1"
+pg_command = "PGPASSWORD=7OqRu1iHlVeLB7O8suzjZRl8MUxqGkeN psql -h dpg-clavig616hkc73816s70-a.frankfurt-postgres.render.com -U test test_pan1"
 def create_app():
     app = Flask(__name__)
     app.config["PROPAGETE_EXCEPTIONS"] = True
@@ -24,13 +34,13 @@ def create_app():
     app.config[
         "OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
     app.config[
-        "SQLALCHEMY_DATABASE_URI"] = "postgresql://eclecticiq:test@localhost:5432/test"
+        "SQLALCHEMY_DATABASE_URI"] = external_url
     app.config[
         'SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Ovo se često postavlja na False da bi se izbegli upozorenja
     app.config['JWT_SECRET_KEY'] = "rest_api"
     db.init_app(app)
     api = Api(app)
-
+    migrate = Migrate(app,db)
     jwt = JWTManager(app)
     @jwt.token_in_blocklist_loader
     def check_if_toke_is_in_blocklist(jwt_header, jwt_payload):
@@ -48,9 +58,9 @@ def create_app():
     #     if identity == 1:
     #         return {"is_admin": True}
     #     return {"is_admin": False}
-
-    with app.app_context():
-        db.create_all()
+    # We are using now migrate :D
+    # with app.app_context():
+    #     db.create_all()
 
     api.register_blueprint(ItemBlueprint)
     api.register_blueprint(TagBlueprint)
--- models/user.py
+++ models/user.py
@@ -6,4 +6,5 @@ class UserModel(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
+    is_admin = db.Column(db.Boolean)
     password = db.Column(db.String(80), nullable=False)
